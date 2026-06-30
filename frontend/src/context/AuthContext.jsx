72c8574953d1c79ea25bf7c05fba546ddb41b8dc
mohiniy166-maker <mohiniy166@gmail.com>
2026-06-30 05:50:27 +0530

    fix: add /api to base URL

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -6,7 +6,8 @@ const AuthContext = createContext();
 // const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 const API_URL =
   import.meta.env.VITE_API_URL ||
-  "https://hackathon-portal-production.up.railway.app";
+  "https://hackathon-portal-production.up.railway.app/api";
+
 
 // Create configured Axios instance
 export const api = axios.create({ baseURL: API_URL });
